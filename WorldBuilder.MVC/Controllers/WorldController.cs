d058df023e4c3278d6263ff72d141e0ceeadfffc
Tom Jarzyniecki III <tjjarz@gmail.com>
2020-04-02 00:26:45 -0400

    create page has cool dynamic fields!

---
+++ WorldBuilder.MVC/Controllers/WorldController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace WorldBuilder.MVC.Controllers
+{
+    public class WorldController : Controller
+    {
+        public XDocument DocGrabber()
+        {
+            //var filepath = HttpContext.Current.Server.MapPath("~/WorldBuilder.Data/WorldData.xml");
+            var filepath = "C:/Users/Tom/OneDrive/Codespace/Red/WorldBuilder/WorldBuilder.Data/WorldData.xml";
+            XDocument loaded_xdoc = XDocument.Load(filepath);
+            return loaded_xdoc;
+        }
+
+        // GET: World
+        public ActionResult Index()
+        {
+            XDocument xdoc = DocGrabber();
+            return View(xdoc);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        public ActionResult Create(XElement xtree)
+        {
+            XDocument xdoc = DocGrabber();
+
+            xdoc.Add(xtree);
+            
+            return View(xtree);
+        }
+    }
+}
---
+++ WorldBuilder.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorldBuilder.MVC.Startup))]
+namespace WorldBuilder.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
