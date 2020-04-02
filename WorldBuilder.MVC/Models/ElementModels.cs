206f418659e9a239a96252728933e15a71e1ccaa
Tom Jarzyniecki III <tjjarz@gmail.com>
2020-04-02 19:08:22 -0400

    madness?

---
+++ WorldBuilder.MVC/Models/ElementModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorldBuilder.MVC.Models
+{
+    public class Element
+    {
+        [Required]
+        public string XLName { get; set; }
+        public string XLValue { get; set; }
+
+    }
+
+    public class TextElement : Element
+    {
+        public string XLNameFormatting { get; set; }
+        public string XLValueFormatting { get; set; }
+    }
+
+    public class ListElement : Element
+    {
+        //public string XLName { get; set; }
+
+        //new public string XLValue { get; } //here to hopefully keep me from accidentally giving this thing a value
+
+        public IEnumerable<Element> Elements { get; set; }
+
+    }
+
+    public class TextBlock : Element
+    {
+        //public string XLName { get; set; }
+
+        public IEnumerable<TextElement> Elements { get; set; }
+
+    }
+
+    public class Gaggle
+    {
+        public IEnumerable<Element> Elements {get;set;}
+    }
+}
